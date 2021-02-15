cbd2a07b878e87f62b7796f4cce1f2da7d544014
brunamello <brunacrespomello@gmail.com>
2021-02-15 18:50:44 +0000

    02. Thymeleaf e Bootstrap

---
+++ src/main/java/com/brunamello/mvc/mudi/model/Pedido.java
@@ -0,0 +1,62 @@
+package com.brunamello.mvc.mudi.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Pedido {
+    private String nomeProduto;
+    private BigDecimal valorNegociado;
+    private LocalDate data;
+    private String urlProduto;
+    private String urlImagem;
+    private String descricao;
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public BigDecimal getValorNegociado() {
+        return valorNegociado;
+    }
+
+    public void setValorNegociado(BigDecimal valorNegociado) {
+        this.valorNegociado = valorNegociado;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public String getUrlProduto() {
+        return urlProduto;
+    }
+
+    public void setUrlProduto(String urlProduto) {
+        this.urlProduto = urlProduto;
+    }
+
+    public String getUrlImagem() {
+        return urlImagem;
+    }
+
+    public void setUrlImagem(String urlImagem) {
+        this.urlImagem = urlImagem;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
