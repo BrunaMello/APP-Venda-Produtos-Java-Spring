fb67f242a79478b0438ed70d0d82f93d3098b697
brunamello <brunacrespomello@gmail.com>
2021-02-18 08:51:44 +0000

    04. Trabalhando com Formulario

---
+++ src/main/java/com/brunamello/mvc/mudi/dto/RequisicaoNovoPedido.java
@@ -0,0 +1,53 @@
+package com.brunamello.mvc.mudi.dto;
+
+import com.brunamello.mvc.mudi.model.Pedido;
+
+public class RequisicaoNovoPedido {
+
+    private String nomeProduto;
+    private String urlProduto;
+    private String imgProduto;
+    private String descProduto;
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public String getUrlProduto() {
+        return urlProduto;
+    }
+
+    public void setUrlProduto(String urlProduto) {
+        this.urlProduto = urlProduto;
+    }
+
+    public String getImgProduto() {
+        return imgProduto;
+    }
+
+    public void setImgProduto(String imgProduto) {
+        this.imgProduto = imgProduto;
+    }
+
+    public String getDescProduto() {
+        return descProduto;
+    }
+
+    public void setDescProduto(String descProduto) {
+        this.descProduto = descProduto;
+    }
+
+    public Pedido toPedido() {
+        Pedido pedido = new Pedido();
+        pedido.setNomeProduto(nomeProduto);
+        pedido.setUrlProduto(urlProduto);
+        pedido.setUrlImagem(imgProduto);
+        pedido.setDescricao(descProduto);
+        return pedido;
+
+    }
+}
